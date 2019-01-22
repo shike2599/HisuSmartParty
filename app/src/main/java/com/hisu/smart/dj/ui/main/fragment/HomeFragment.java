1b8c33aa42ae273e00447a1d8ddc0adab94b75e6
shike_mac <shike@chinahisu.com>
2019-01-22 15:47:38 +0800

    轮播图完成

--- app/src/main/java/com/hisu/smart/dj/ui/main/fragment/HomeFragment.java
+++ app/src/main/java/com/hisu/smart/dj/ui/main/fragment/HomeFragment.java
@@ -1,43 +1,65 @@
 package com.hisu.smart.dj.ui.main.fragment;
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.hisu.smart.dj.R;
+import com.hisu.smart.dj.ui.widget.BannerWidget;
+import com.jaydenxiao.common.base.BaseFragment;
+import com.jaydenxiao.common.commonutils.LogUtils;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+import com.youth.banner.loader.ImageLoader;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * 首页
  * @author lichee
  */
-public class HomeFragment extends Fragment {
-
+public class HomeFragment extends BaseFragment {
+    private String TAG = "HomeFragment";
+    private Banner homeBanner;
+    private List<Integer> homeBannerImages;
+    private int[] Images = {
+            R.mipmap.home_selected_ico,R.mipmap.study_selected_icon,
+            R.mipmap.dang_selected_icon,R.mipmap.doit_selected_icon};
     public HomeFragment() {
+
+    }
+
+
+    private void initData() {
+        homeBannerImages = new ArrayList<>();
+        for(int i = 0; i < Images.length; i++){
+            homeBannerImages.add(Images[i]);
+        }
+    }
+
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.fragment_home;
+    }
+    //在initView之前调用
+    @Override
+    public void initPresenter() {
+        initData();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void initView() {
+       homeBanner = rootView.findViewById(R.id.home_banner);
+       BannerWidget.setBanner(homeBanner,homeBannerImages);
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-    }
-
 }
---
+++ app/src/main/java/com/hisu/smart/dj/ui/widget/BannerWidget.java
@@ -0,0 +1,71 @@
+package com.hisu.smart.dj.ui.widget;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+import com.youth.banner.loader.ImageLoader;
+
+import java.util.List;
+
+/**
+ * Banner填充数据
+ */
+public class BannerWidget {
+
+    public static void setBanner(Banner banner, List<Integer> dataSize){
+        //设置样式,默认为:Banner.NOT_INDICATOR(不显示指示器和标题)
+        //可选样式如下:
+        //1. Banner.CIRCLE_INDICATOR    显示圆形指示器
+        //2. Banner.NUM_INDICATOR   显示数字指示器
+        //3. Banner.NUM_INDICATOR_TITLE 显示数字指示器和标题
+        //4. Banner.CIRCLE_INDICATOR_TITLE  显示圆形指示器和标题
+        banner.setImageLoader(new GlideImageLoader());
+        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR);
+        //设置图片加载器  加载网络图片
+//        homeBanner.setImageLoader(new GlideImageLoader());
+        //设置图片集合
+        banner.setImages(dataSize);
+        //设置banner动画效果
+        banner.setBannerAnimation(Transformer.DepthPage);
+        //设置标题集合（当banner样式有显示title时）
+//        homeBanner.setBannerTitles(titles);
+        //设置自动轮播，默认为true
+        banner.isAutoPlay(true);
+        //设置轮播时间
+        banner.setDelayTime(3000);
+        //设置轮播样式（没有标题默认为右边,有标题时默认左边）
+        //可选样式:
+        //Banner.LEFT   指示器居左
+        //Banner.CENTER 指示器居中
+        //Banner.RIGHT  指示器居右
+        banner.setIndicatorGravity(BannerConfig.CENTER);
+        //banner设置方法全部调用完毕时最后调用
+        banner.start();
+    }
+    //Banner  重写图片加载器 网络图片需要重写
+    private static class GlideImageLoader extends ImageLoader {
+        @Override
+        public void displayImage(Context context, Object path, ImageView imageView) {
+            /**
+             注意：
+             1.图片加载器由自己选择，这里不限制，只是提供几种使用方法
+             2.返回的图片路径为Object类型，由于不能确定你到底使用的那种图片加载器，
+             传输的到的是什么格式，那么这种就使用Object接收和返回，你只需要强转成你传输的类型就行，
+             切记不要胡乱强转！
+             */
+            //Glide 加载图片简单用法
+//            Glide.with(context).load(path).into(imageView);
+//            //用fresco加载图片简单用法，记得要写下面的createImageView方法
+//            Uri uri = Uri.parse((String) path);
+//            imageView.setImageURI(uri);
+
+            Glide.with(context.getApplicationContext())
+                    .load(path)
+                    .into(imageView);
+        }
+    }
+}
