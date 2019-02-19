68f83e11a4b98055fa8b1ed2fd4f00a48e8c80c1
lizh <lichee@qq.com>
2019-02-19 11:11:23 +0800

    更换网动视频会议sdk,解决了登陆闪屏问题

--- app/src/main/java/com/hisu/smart/dj/ui/iactive/activity/IactiveLoginActivity.java
+++ app/src/main/java/com/hisu/smart/dj/ui/iactive/activity/IactiveLoginActivity.java
@@ -5,7 +5,6 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -19,33 +18,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.hisu.smart.dj.R;
-import com.hisu.smart.dj.app.AppConfig;
-import com.hisu.smart.dj.ui.iactive.utils.ConfigService;
-import com.hisu.smart.dj.ui.iactive.utils.StartMeetingUtils;
-import com.hisu.smart.dj.ui.iactive.utils.StringUtils;
 import com.hisu.smart.dj.ui.iactive.view.TitleBarView;
 import com.jaydenxiao.common.commonutils.StatusBarUtil;
 import com.wdliveuc.android.ActiveMeeting7.ActiveMeeting7Activity;
 import com.wdliveuc.android.ActiveMeeting7.BaseActivity;
-import com.wdliveuc.android.ActiveMeeting7.ClassList.ClassInfo;
-import com.wdliveuc.android.ActiveMeeting7.Login1Activity;
 import com.wdliveuc.android.domain.ConfigEntity;
-
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import com.wdliveuc.android.domain.ConfigService;
 
 import cn.com.iactive.utils.CommonUtil;
 import cn.com.iactive.utils.Constant;
-import cn.com.iactive.utils.MeetingListBack;
+import cn.com.iactive.utils.StartMeeting;
+import cn.com.iactive.utils.immStringUtils;
 import cn.com.iactive.vo.MeetingInfo;
-import cn.com.iactive.vo.Room;
 
-
-/**
- * @author lichee
- */
 public class IactiveLoginActivity extends BaseActivity {
 
 	private Button mLogin;
@@ -57,23 +42,33 @@ public class IactiveLoginActivity extends BaseActivity {
 	public static String iac_ser_ip="";
 	public static String iac_username="";
 	public static String iac_password="";
+
     private ImageView back_img;
     private TextView title_text;
+
+	public static boolean m_ShowJoinRoomList=false;
+
+
+
 	public static void startAction(Activity activity){
 		Intent intent = new Intent(activity, IactiveLoginActivity.class);
 		activity.startActivity(intent);
 	}
 
-			
+
+
+
 	@Override
 	public void onClick(View v) {
+		m_ShowJoinRoomList=false;
 		switch (v.getId()) {
 		case R.id.firm_login	:
+			m_ShowJoinRoomList=false;
 			String firmName = et_firm_name.getText().toString().trim();
 			String username = et_username.getText().toString().trim();
 			String password = et_password.getText().toString().trim();
-
-			if (StringUtils.isBlank(firmName)) {
+			
+			if (immStringUtils.isBlank(firmName)) {
 				CommonUtil.showToast(this,
 						getString(R.string.login_firm_name_isnull),
 						Toast.LENGTH_LONG);
@@ -90,37 +85,63 @@ public class IactiveLoginActivity extends BaseActivity {
 						Toast.LENGTH_LONG);
 				return;
 			}
-			if(!StringUtils.isNumberOrChar(password)){
-				CommonUtil.showToast(this,getString(R.string.login_pass_is_only_number_or_char), Toast.LENGTH_LONG);
-				return;
-			}
+//			if(!immStringUtils.isNumberOrChar(password)){
+//				CommonUtil.showToast(this,getString(R.string.login_pass_is_only_number_or_char), Toast.LENGTH_LONG);
+//				return;
+//			}
 			if(!ActiveMeeting7Activity.GetClearGCFinish()){
 				CommonUtil.showToast(this,"没有清理完全内存", Toast.LENGTH_LONG);
 				return;
 			}
 
-			try {
-				String enterprisename = firmName;
-				String strname = username;
-			} catch (Exception e) {
+			SharedPreferences.Editor ed = sp.edit();
+			iac_ser_ip=firmName;
+			iac_username=username;
+			iac_password=password;
+			ed.putString("enterprisename", "");
+			ed.putString("join_et_server1", iac_ser_ip);
+			ed.putString("join_et_nickname", iac_username);
+			ed.putString("join_et_roompass", iac_password);
+			ed.commit();
+			StartMeeting startMeeting = new StartMeeting();
+	        MeetingInfo meetingInfo = new MeetingInfo();
+	        if(iac_ser_ip.equals("master.liveuc.net")) {
+				meetingInfo.head = Constant.HEAD_LIVEUC;
+				meetingInfo.serverUTF8 = 1;
+			}else {
+				meetingInfo.head = Constant.HEAD_ACTIVEMEETING;
+				meetingInfo.serverUTF8 = 0;
 			}
-			iac_ser_ip = firmName;
-			iac_username = username;
-			iac_password = password;
-			AppConfig.getInstance().setString("enterprisename", "");
-			AppConfig.getInstance().setString("join_et_server1", iac_ser_ip);
-			AppConfig.getInstance().setString("join_et_nickname", iac_username);
-			AppConfig.getInstance().setString("join_et_roompass", iac_password);
-			StartMeetingUtils startMeeting = new StartMeetingUtils();
-			MeetingInfo info = new MeetingInfo();
-			info.head = Constant.HEAD_ACTIVEMEETING;
-			info.srvIP = firmName;
-			info.enterprisename = "";
-			info.username = username;
-			info.userpass = password;
-			info.isAnonymous = 0;
-			info.serverUTF8 = 0;
-			startMeeting.start(IactiveLoginActivity.this, info);
+	        
+//	        meetingInfo.roomId = room.roomId;
+	        String nickname = username;
+	    	//meetingInfo.username = room.hostName;
+	    	//meetingInfo.userpass = room.hostPass;
+	    	meetingInfo.username = username;
+	    	meetingInfo.userpass = password;
+//	        meetingInfo.nickname = nickname;
+	        meetingInfo.isAnonymous=0;
+	        meetingInfo.srvIP=firmName;
+	        if(nickname.contains("@")){
+				String[] strserver = ActiveMeeting7Activity.getNode(nickname, "@");
+				if (strserver != null&&strserver.length==2) {
+					nickname=strserver[1]+"|"+strserver[0];
+				}
+			}
+            meetingInfo.nickname = nickname;
+	        startMeeting.start(IactiveLoginActivity.this,meetingInfo);
+			
+//			StartMeetingUtils startMeeting = new StartMeetingUtils();
+//			MeetingInfo info=new MeetingInfo();
+//			info.head=Constant.HEAD_ACTIVEMEETING;
+//			info.srvIP=firmName;
+//			info.enterprisename="";
+//			info.username=username;
+//			info.userpass=password;
+//			info.isAnonymous=0;
+//			info.serverUTF8=0;
+//			info.roomId=3;
+//			startMeeting.start(IactiveLoginActivity.this, info);
 //			String meetingUrl=startMeeting.getMeetingUrl(info); 
 //			Intent intent = new Intent();
 //	        intent.setClass(IactiveLoginActivity.this, ActiveMeeting7Activity.class);
@@ -160,57 +181,36 @@ public class IactiveLoginActivity extends BaseActivity {
 		title_text = findViewById(R.id.title_TextView);
 		initTitle();
 		configEntity = ConfigService.LoadConfig(this);
-		configEntity.isLiveUC = false;
+		configEntity.isLiveUC=false;
 		ConfigService.SaveConfig(this, configEntity);
-		Login1Activity.m_MeetingListBack = new MeetingListBack() {
-			@Override
-			public void finishMeetingListBack() {
-				// TODO Auto-generated method stub
-				
-				List<Room> roomList=new ArrayList<Room>();
-    			List<ClassInfo> classList = null;
-    			if(ActiveMeeting7Activity.mClassList.roomList.size()>0){
-    				for(ClassInfo info: ActiveMeeting7Activity.mClassList.roomList){
-    						Room room=new Room();
-    						room.roomId=info.roomid;
-    						room.roomName=info.roomName;
-    						room.roomPass=info.roomPass;
-    						room.nTOnline=info.nTOnline;
-    						room.onlineUser=info.nOnlineUser;
-    						room.nTotalUser=info.nTotalUser;
-    						roomList.add(room);
-    				}
-    			}
-    			Intent intent=new Intent(Login1Activity.m_LoginActivity,JoinRoomListActivity.class);
-    			intent.putExtra("roomList", (Serializable)roomList);
-    			startActivityForResult(intent, Login1Activity.REQ_LOGIN_list);
-			}
-			//点击上传图片之后的响应事件
-			@Override
-			public void ClickUploadPhoto() {
-				// TODO Auto-generated method stub
-				Log.i("", "interface ClickUploadPhoto 点击");
-				//判断这个activity是否为控，如果不为空，可以通过这个Activity跳转：ActiveMeeting7Activity.m_ActiveMeeting7Activity
-			}
-			//点击上传图片之后的响应事件
-			@Override
-			public void Medicaladvice() {
-				// TODO Auto-generated method stub
-				Log.i("", "interface inMedicaladvice 点击");
-				//判断这个activity是否为控，如果不为空，可以通过这个Activity跳转：ActiveMeeting7Activity.m_ActiveMeeting7Activity
-			}
-			@Override
-			public void Meetinglistback() {
-				// TODO Auto-generated method stub
-				//如果没有传会议室id。则显示会议室列表。否则。直接入会
-			}
-			//用户退出会议室
-			@Override
-			public void MeetingExit() {
-				// TODO Auto-generated method stub
-				Log.i("", "interface MeetingExit 退会");
-			}
-		};
+		//如果没有传会议室id。则会返回会议室列表界面。
+//		Login1Activity.m_MeetingListBack=new MeetingListBack() {
+//			
+//			@Override
+//			public void finishMeetingListBack() {
+//				// TODO Auto-generated method stub
+//			}
+//			//暂时没用
+//			@Override
+//			public void ClickUploadPhoto() {
+//				// TODO Auto-generated method stub
+//			}
+//			//暂时没用
+//			@Override
+//			public void Medicaladvice() {
+//				// TODO Auto-generated method stub
+//			}
+//			@Override
+//			public void Meetinglistback() {
+//				// TODO Auto-generated method stub
+//				//如果没有传会议室id。则显示会议室列表。否则。直接入会
+//			}
+//			//用户退出会议室
+//			@Override
+//			public void MeetingExit() {
+//				// TODO Auto-generated method stub
+//			}
+//		};
 	}
 
 	@Override
@@ -220,10 +220,11 @@ public class IactiveLoginActivity extends BaseActivity {
 
 	@Override
 	protected void processLogic() {
+		sp = getSharedPreferences("loginInfo",MODE_PRIVATE);
 		tv_login_version.setText(this.getString(R.string.setting_version)+ CommonUtil.getAppVersionName(this));
-		iac_ser_ip =  AppConfig.getInstance().getString("join_et_server1", "mcu.iactive.com.cn");
-		iac_username = AppConfig.getInstance().getString("join_et_nickname", "");
-		iac_password = AppConfig.getInstance().getString("join_et_roompass", "");
+		iac_ser_ip = sp.getString("join_et_server1", "mcu.iactive.com.cn");
+		iac_username = sp.getString("join_et_nickname", "");
+		iac_password = sp.getString("join_et_roompass", "");
 		et_firm_name.setText(iac_ser_ip);
 		et_username.setText(iac_username);
 		et_password.setText(iac_password);
@@ -244,26 +245,37 @@ public class IactiveLoginActivity extends BaseActivity {
 //		mTitleBarView.setVisibility(View.VISIBLE);
 //		mTitleBarView.setCommonTitle(View.VISIBLE);
 		mTitleBarView.setTitleText(R.string.login_memu_title);
-		mTitleBarView.setComeBackOnclickListener(comeBackClickListener);
+//		mTitleBarView.setComeBackOnclickListener(comeBackClickListener);
 	}
 	
 	private OnClickListener comeBackClickListener = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
+			//Intent intent = new Intent(this,MainActivity.class);
+			//startActivity(intent);
 			comeback();
 		}
 	};
 
 	
-    @Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
-        	 comeback();
+        	finish();
+//        	System.exit(0);
         }
         return true;
     }
     
-    private void comeback(){
+    
+    
+    @Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		System.exit(0);
+	}
+
+	private void comeback(){
     	finish();
     }
     private void hiddenInputMode() {
